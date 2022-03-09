70f84bf6b45660ceaa9e21216952d38f231fd1c5
MEBAREK <21114000@ppti-14-407-01.ufr-info-p6.jussieu.fr>
2022-03-09 11:38:28 +0100

    eval expr - reste app + appr

--- eval.ml
+++ eval.ml
@@ -9,15 +9,56 @@
 (* ========================================================================== *)
 open Ast
 
-
 type v = 
     | Z of int
     | F of expr*string list*(string*v) list
     | Fr of expr*string*string list*(string*v) list
 
-let (ev_env:(string*v) list) = [];;
+let (ev_env:(string*v) list) = [("true",Z(1));("false",Z(0))];;
 
 
+(* ------------------------------------------------ *)
+
+let rec find_x x e = 
+  match e with 
+   [] -> None
+  | (a,v)::rest -> if a == x then Some v else find_x x rest
+
+(* ------------------------------------------------ *)
+
+let fromSome x = match x with 
+  Some v -> v
+
+(* ------------------------------------------------ *)
+
+let eval_not x = match x with
+  Z(0) -> Z(1)
+  |Z(1) -> Z(0)
+
+let eval_add x y = match x,y with
+Z(x1),Z(y1) -> Z(x1+y1)
+
+let eval_sub x y = match x,y with
+Z(x1),Z(y1) -> Z(x1-y1)
+
+let eval_mul x y = match x,y with
+Z(x1),Z(y1) -> Z(x1*y1)
+
+let eval_div x y = match x,y with
+Z(x1),Z(y1) -> Z(x1/y1)
+
+let eval_eq x y = match x,y with
+Z(x1),Z(y1) -> if(x1==y1) then Z(1) else Z(0)
+
+let eval_lt x y = match x,y with
+Z(x1),Z(y1) -> if(x1<y1) then Z(1) else Z(0)
+
+let rec extractArgs l= match l with
+  [] -> []
+  |Argu(a,t)::xs -> a::(extractArgs xs)
+  
+
+(* ------------------------------------------------ *)
 let rec eval_arg a = match a with 
   Argu(i,t) -> (
   ) and  eval_args a = match a with
@@ -31,15 +72,26 @@ let rec eval_arg a = match a with
       in
      ()
     
-  ) and eval_expr e c=
-  match e with
-      ASTNum n -> Z(int_of_string n)
-    | ASTId x -> ()
-    | ASTApp(e, es) -> ()
-    | ASTif(condition,body,alternant) -> ()
-    |ASTfun(args,e) -> ()
-    |ASTand(a,b) -> ()
-    |ASTor(a,b) -> ()
+  ) and eval_expr e c =  match e with
+      ASTNum n -> Z(n)
+    | ASTId x -> fromSome (find_x x c)
+    | ASTApp(e, es) -> 
+      (match e with 
+        ASTId("not") -> eval_not ((eval_expr (List.hd es) c)) 
+       |ASTId("add") ->  (eval_add (eval_expr (List.hd es) c) (eval_expr (List.hd (List.tl es)) c))
+       |ASTId("sub") ->  (eval_sub (eval_expr (List.hd es) c) (eval_expr (List.hd (List.tl es)) c))
+       |ASTId("mul") ->  (eval_mul (eval_expr (List.hd es) c) (eval_expr (List.hd (List.tl es)) c))
+       |ASTId("div") ->  (eval_div (eval_expr (List.hd es) c) (eval_expr (List.hd (List.tl es)) c))
+       |ASTId("eq") ->  (eval_eq (eval_expr (List.hd es) c) (eval_expr (List.hd (List.tl es)) c))
+       |ASTId("lt") ->  (eval_lt (eval_expr (List.hd es) c) (eval_expr (List.hd (List.tl es)) c))
+
+
+
+      )
+    |ASTif(condition,body,alternant) -> if ((eval_expr condition c )== Z(1)) then (eval_expr body c) else (eval_expr alternant c)
+    |ASTfun(args,e) -> F(e,(extractArgs args),c)
+    |ASTand(a,b) -> if (eval_expr a c) == Z(1) then (eval_expr b c) else Z(0)
+    |ASTor(a,b) -> if (eval_expr a c) == Z(0) then (eval_expr b c) else Z(1)
 and eval_exprs es =
   match es with
       [] -> ()
